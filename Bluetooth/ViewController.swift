b9f6ba241ca593e6cae9c0986eac6f39b84aa4e2
Hyebin Kim <developerhyebin@gmail.com>
2023-07-26 14:36:12 +0900

    프로젝트 생성

--- Bluetooth/ViewController.swift
+++ Bluetooth/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    private var peripherals: [CBPeripheral] = []
+    private var centralManager: CBCentralManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-
 }
 
+extension ViewController : CBPeripheralDelegate, CBCentralManagerDelegate{
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("unknown")
+        case .resetting:
+            print("restting")
+        case .unsupported:
+            print("unsupported")
+        case .unauthorized:
+            print("unauthorized")
+        case .poweredOff:
+            print("power Off")
+        case .poweredOn:
+            print("power on")
+        @unknown default:
+            fatalError()
+        }
+    }
+}
